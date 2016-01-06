a581ed3d82dec54603cf5923c97fb1df887729d0
Diego Laucsen da Rosa <diego.laucsen@pixeon.com>
2016-01-06 17:36:51 -0200

    Starting with inspector.

--- app/scripts/editor/editor.controller.js
+++ app/scripts/editor/editor.controller.js
@@ -1,30 +1,6 @@
 'use strict';
 
 angular.module('samples-module')
-    .factory('editor-sampler', [
-        function () {
-
-            function get(type) {
-                if (type === 'label') {
-                    return {
-                        title: 'Label',
-                        type: 'label',
-                        name: '',
-                        data: 'Label',
-                        style: {
-                            color: '#110022',
-                            'font-size': '12px'
-                        }
-                    };
-                }
-                return undefined;
-            }
-
-            return {
-                get: get
-            };
-        }
-    ])
     .controller('editor-controller', [
         '$scope',
 
@@ -38,6 +14,11 @@ angular.module('samples-module')
             $scope.widgets = Info.getAvailableComponents();
 
             $scope.rootElement = [];
+            $scope.activeElement = undefined;
+
+            $scope.editingfields = ['title', 'type', 'name', 'data', 'style'];
+            $scope.editablesFields = ['name', 'data'];
+            $scope.specialFields = ['style'];
 
             $scope.edDragoverCallback = function (type) {
                 var lcondition = $scope.rootElement.length === 0;
@@ -47,6 +28,28 @@ angular.module('samples-module')
             $scope.edDropCallback = function (type) {
                 var sampled = Sampler.get(type);
                 return sampled || false;
+            };
+
+            $scope.isEditable = function(field) {
+                return $scope.editablesFields.indexOf(field) >= 0;
+            };
+            $scope.isSpecial = function(field) {
+                return $scope.specialFields.indexOf(field) >= 0;
+            };
+
+            $scope.rootClicked = function() {
+                $scope.rootElement[0].active = !$scope.rootElement[0].active;
+                if ($scope.rootElement[0].active) {
+                    $scope.activeElement = $scope.rootElement[0];
+                } else {
+                    $scope.activeElement = undefined;
+                }
+            };
+
+            $scope.runSpecial = function(field) {
+                if (field === 'style') {
+                    console.log($scope.activeElement[field]);
+                }
             };
         }
     ]);
